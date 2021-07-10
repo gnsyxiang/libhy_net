4d7c2071bc5da01b2da1c2fa4cea24300c56ea4d
zhenquan.qiu <gnsyxiang@163.com>
2021-07-10 15:17:41 +0800

    refactor: refactor main.c with hy_module.h

--- example/main.c
+++ example/main.c
@@ -19,19 +19,50 @@
  */
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "hy_net.h"
 
+#include "hy_utils/hy_module.h"
 #include "hy_utils/hy_log.h"
 #define ALONE_DEBUG 1
+
+typedef struct {
+    void *log_handle;
+    void *net_handle;
+} _main_context_t;
 
 static void _data_cb(void *data, size_t len, void *args)
 {
     LOGD("len: %d, data: %s \n", len, data);
 }
 
-static void *_connect_server(void)
+static void _module_destroy(_main_context_t *context)
 {
+    // note: 增加或删除要同步到module_create_t中
+    module_destroy_t module[] = {
+        {"log",     context->log_handle,    HyLogDestroy},
+        {"net",     context->net_handle,    HyNetDestroy},
+    };
+
+    RUN_DESTROY(module);
+}
+
+static _main_context_t *_module_create(void)
+{
+    _main_context_t *context = malloc(sizeof(*context));
+    if (!context) {
+        LOGE("malloc faild \n");
+        return NULL;
+    }
+    memset(context, '\0', sizeof(*context));
+
+    HyLogConfig_t log_config;
+    log_config.buf_len = 512;
+    log_config.level = HY_LOG_LEVEL_INFO;
+    log_config.config_file = "./res/config/log4cplus.rc";
+
     HyNetConfig_t net_config;
     net_config.ip           = "192.168.1.57";
     net_config.port         = 7809;
@@ -39,37 +70,30 @@ static void *_connect_server(void)
     net_config.config_save.data_cb  = _data_cb;
     net_config.config_save.args     = NULL;
 
-    void *handle = HyNetCreate(&net_config);
-    if (!handle) {
-        LOGE("HyNetCreate faild \n");
-        return NULL;
-    }
-    return handle;
+    // note: 增加或删除要同步到module_destroy_t中
+    module_create_t module[] = {
+        {"log",     context->log_handle,    &log_config,    (create_t)HyLogCreate,  HyLogDestroy},
+        {"net",     context->net_handle,    &net_config,    (create_t)HyNetCreate,  HyNetDestroy},
+    };
+
+    RUN_CREATE(module);
+
+    return context;
 }
 
 int main(int argc, char const* argv[])
 {
-    HyLogCreate(HY_LOG_LEVEL_ALL, 1024, "./res/config/log4cplus.rc");
-
-    void *handle = _connect_server();
+    _main_context_t *context = _module_create();
+    if (!context) {
+        LOGE("_module_create faild \n");
+        return -1;
+    }
 
     while (1) {
         sleep(1);
     }
 
-    int cnt = 0;
-    while (cnt < 3) {
-        if (0 != HyNetProcess(handle)) {
-            LOGI("disconnect server \n");
-
-            handle = _connect_server();
-            cnt++;
-        }
-    }
-
-    HyNetDestroy(handle);
-
-    HyLogDestroy();
+    _module_destroy(context);
 
     return 0;
 }
